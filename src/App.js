b3681a5b822effb5bdb44f6dc858e01a262d64ea
AaronMullan <aaroncmullan@gmail.com>
2020-06-02 16:49:30 -0700

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['one', 'two', 'three']);
+  const [value, setValue] = useState('');
+  const handleChange = (e) => {setValue(e.target.value)};
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addTodo(value)
+  }
+  const addTodo = (value) => {
+    const newTodos = [...todos, value]
+    setTodos(newTodos);
+  }
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Cool Todos</h1>
+      <ul>
+        {todos.map((todo, index) =>
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>delete</button>
+            </li>
+          )}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text"
+        value={value}
+        onChange={handleChange}
+        />
+        <input 
+        type="submit"
+        value="Submit"
+        />
+      </form>
     </div>
   );
 }
